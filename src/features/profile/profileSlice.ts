5ac398e6140c06f450e17182d14b7021d8ba6078
Sebastian Garcia ITG <sebastian.garcia@itglobers.com>
2025-10-27 04:30:24 -0300

    feat(auth): implement session management with getSessionThunk and update auth state feat(posts): create postsSlice for managing posts, drawer content, and comments feat(profile): enhance profileSlice to manage user likes and session data

--- src/features/profile/profileSlice.ts
+++ src/features/profile/profileSlice.ts
@@ -1,21 +1,44 @@
 import {createSlice} from '@reduxjs/toolkit';
+import { likePost } from '../commonActions';
+import { getSessionThunk } from '../auth/authSlice';
+
+const INITIAL_STATE: ProfileState = {
+        name: '',
+        email: '',
+        id: '',
+        image: '',
+        likedPosts: []
+    }
+
 
 const profileSlice = createSlice({
     name: 'profile',
-    initialState: {
-        name: 'Sebastian garcia',
-        email: 'sebastian@example.com',
-    },
+    initialState: INITIAL_STATE,
     reducers: {
-        setName: (state, action) => {
-            state.name = action.payload;
+        loadLikes: (state, action) => {
+            state.likedPosts = action.payload 
         },
-        setEmail: (state, action) => {
-            state.email = action.payload;
-        }
+    },
+    extraReducers:(builder) => {
+        builder
+            .addCase(likePost, (state, action) => {
+                if(action.payload.isLiked){
+                    state.likedPosts = state.likedPosts.filter(post => post !== action.payload.post_id)
+                } else {
+                    state.likedPosts = [action.payload.post_id, ...state.likedPosts]
+                }
+                localStorage.setItem(`user-likes-${state.id}`, JSON.stringify(state.likedPosts))
+            })
+            .addCase(getSessionThunk.fulfilled, (state,action) => {
+                const session = action.payload ?? {user: INITIAL_STATE}
+                state.name = session.user?.name ?? '';
+                state.id = session.user?.id ?? '';
+                state.image = session.user?.image ?? '';
+                state.email = session.user?.email ?? '';
+            })
     }
 });
 
-export const { setName, setEmail } = profileSlice.actions;
+export const { loadLikes } = profileSlice.actions;
 
 export default profileSlice.reducer;
