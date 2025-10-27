91a20d66a805a83ac78538bdee6252b8bbda4fef
Sebastian Garcia ITG <sebastian.garcia@itglobers.com>
2025-10-27 04:21:04 -0300

    feature (config) : add remote image patterns to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // Specify the protocol (e.g., 'https')
+        hostname: 'pbs.twimg.com', // Specify the exact hostname
+        pathname: '/profile_images/**',
+      },
+      new URL('https://xsgames.co/randomusers/assets/avatars/female/**'),
+      new URL('https://xsgames.co/randomusers/assets/avatars/male/**'),
+      new URL('https://picsum.photos/id/**/200/300.webp'),
+    ]
+  }
 };
 
 export default nextConfig;
