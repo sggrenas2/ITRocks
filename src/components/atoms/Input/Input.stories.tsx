1007176b6be3b6f26c6b35df10db50ebd3b3c060
Sebastian Garcia ITG <sebastian.garcia@itglobers.com>
2025-10-27 04:24:35 -0300

    feat (input): create Input component stories with validation and size options

---
+++ src/components/atoms/Input/Input.stories.tsx
@@ -0,0 +1,88 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { Input } from './index';
+
+const meta = {
+  title: 'Atoms/Input',
+  component: Input,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    (Story) => (
+      <form className="w-96 h-64 flex justify-center items-center bg-oxford-platinum">
+        <Story />
+        <button type='submit' className='ml-2 border border-oxford-blue p-2 flex items-center'>submit</button>
+      </form>
+    ),
+  ],
+  tags: ['autodocs'],
+  argTypes: {
+    label: {
+      control: { type: 'text' },
+      description: 'Etiqueta del campo de entrada',
+      table: {
+        defaultValue: { summary: "''" },
+        type: { summary: 'string' },
+      }
+    },
+    error: {
+      control: { type: 'text' },
+      description: 'Mensaje de error del campo de entrada (se muestra con los errores disparados por la validacion nativa de html)',
+      table: {
+        defaultValue: { summary: "''" },
+        type: { summary: 'string' },
+      }
+    },
+    sizeInput: {
+      control: { type: 'radio' },
+      options: ['full', 'medium', 'small'],
+      description: 'Tamaño del campo de entrada (e.g., small, medium, large)',
+      table: {
+        defaultValue: { summary: "'text'" },
+        type: { summary: 'string' },
+      }
+    },
+  }
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<InputProps>;
+
+export const Default: Story = {
+  args: {
+    label: 'Email',
+    type: 'email',
+    value: 'asdas'
+  },
+};
+
+export const FullSize: Story = {
+  args: {
+    label: 'Full Name',
+    sizeInput: 'full',
+  },
+};
+
+export const MediumSize: Story = {
+  args: {
+    label: 'Username',
+    sizeInput: 'medium',
+  },
+};
+
+export const SmallSize: Story = {
+  args: {
+    label: 'Code',
+    sizeInput: 'small',
+  },
+};
+
+export const WithError: Story = {
+  args: {
+    label: 'Email',
+    placeholder: 'Invalid email',
+    type: 'email',
+    error: 'Please enter a valid email address',
+    required: true,
+  },
+};
