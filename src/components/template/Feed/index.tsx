be152baf594c18b74de834de876ebef1abc505e4
Sebastian Garcia ITG <sebastian.garcia@itglobers.com>
2025-10-27 05:47:38 -0300

    fix(Feed): fix likes didn't load on start

--- src/components/template/Feed/index.tsx
+++ src/components/template/Feed/index.tsx
@@ -1,15 +1,25 @@
 'use client'
 import { useAppSelector } from "@/store/hooks"
+import { useAppDispatch } from "@/store/hooks"
 import PostCard from "@/components/organisms/PostCard"
 import Drawer from "@/components/template/Drawer"
+import { useEffect } from "react"
+import { loadLikes } from "@/features/profile/mockProfileSlice"
 
 export const Feed = () => {
 
     const posts = useAppSelector(state => state.posts.posts)
+    const id = useAppSelector(state => state.auth.id)
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(loadLikes(JSON.parse(localStorage.getItem(`user-likes-${id}`) ?? "[]")))
+    },[dispatch,id])
 
     if(posts.length === 0){
         return <p>No Post Yet</p>
-    }
+    }   
+
 
     return (
         <>
